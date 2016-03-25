949df0e830197d4a26ab005954b2b31e05929e8d
John Bucknam <jbuck594@gmail.com>
2016-03-25 00:48:11 -0400

    Updated /user api
    Added password-hashing as well as fixed the database calls

--- models/database.js
+++ models/database.js
@@ -4,15 +4,16 @@ var connectionString = process.env.DATABASE_URL || 'postgres://jsb:test@localhos
 var client = new pg.Client(connectionString);
 client.connect();
 
-var user = 'CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR(40) UNIQUE NOT NULL, ' +
-                              'email VARCHAR(40) UNIQUE NOT NULL, first_name VARCHAR(40) NOT NULL, ' +
-                              'last_name VARCHAR(40) NOT NULL);';
+var user = 'CREATE TABLE users (username VARCHAR(40) PRIMARY KEY, ' +
+                                'pass VARCHAR(128) NOT NULL, salt VARCHAR(32) NOT NULL,' +
+                                'email VARCHAR(40) UNIQUE NOT NULL, ' + 
+                                'first_name VARCHAR(40) NOT NULL, last_name VARCHAR(40) NOT NULL);';
 var group;
 var user_group_perm;
 var post;
 var comment;
 var tag;
 
-var query = client.query(user + group + user_group_perm + post + comment + tag);
+var query = client.query(user /*+ group + user_group_perm + post + comment + tag*/);
 
 query.on('end', function() { client.end(); });
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,122 @@
 var express = require('express');
 var router = express.Router();
+var passHash = require('password-hash');
 var pg = require('pg');
 
-var conString = '';
+var connectionString = process.env.DATABASE_URL || 'postgres://jsb:test@localhost/nodesconnect';
 
 /* GET user */
-router.get('/', function(req, res, next) {
-  let query = client.query('SELECT first_name,last_name,user_name,email FROM USER WHERE username=' + req.body.username + ';');
-	return query;
+router.get('/get', function(req, res, next) {
+  pg.connect(connectionString, function(err, client, done) {
+    client.query('SELECT first_name, last_name, username, email ' +
+      'FROM users WHERE username = \'' + req.body.username + '\';',
+      function(err, result) {
+        done();
+
+        if(err) {
+          console.error(err);
+          res.sendStatus(406);
+        }else if(!result || result.rows.length === 0) {
+          res.sendStatus(404);
+        }else {
+          res.status(202).send(result.rows[0]);
+        }
+      });
+  })
 });
 
-router.get('/exists', function(req, res, next) {
-  let query = client.query('SELECT username FROM USER WHERE username=' + req.body.username + ';');
-	if (query === null) {
-		return false;
-	}
-	return true;
+router.delete('/delete', function(req, res, next) {
+  pg.connect(connectionString, function(err, client, done) {
+     client.query('SELECT pass, salt FROM users WHERE username = \'' + req.body.username +'\';',
+      function(err, result) {
+        if(err) {
+          console.error(err);
+          res.sendStatus(406);
+        }else if(!result || result.rows.length === 0) {
+          done();
+          res.sendStatus(404);
+        }else {
+          var hashpass = 'sha1$' + result.rows[0].salt + '$1$' + result.rows[0].pass;
+
+          if(passHash.verify(req.body.pass, hashpass)) {
+            client.query('DELETE FROM users WHERE username = \'' + req.body.username +
+            '\' AND pass = \'' + result.rows[0].pass + '\';',
+            function(err, result) {
+              done();
+
+              if(err) {
+                console.error(err);
+                res.sendStatus(406);
+              } else {
+                res.sendStatus(202);
+              }
+            });
+          }else {
+            res.sendStatus(406);
+          }
+        }
+      });
+  });
 });
 
-router.get('/exists/email', function(req, res, next) {
-  let query = client.query('SELECT username FROM USER WHERE username=' + req.body.email + ';');
-	if (query === null) {
-		return false;
-	}
-	return true;
+router.put('/create', function(req, res, next) {
+  pg.connect(connectionString, function(err, client, done) {
+    var hashlist = passHash.generate(req.body.pass).split('$');
+    pass = hashlist[3];
+    salt = hashlist[1];
+    client.query('INSERT INTO users VALUES (\'' + req.body.username + '\', \'' +
+      pass + '\', \'' + salt + '\', \'' +
+      req.body.email + '\', \'' + req.body.first_name + '\', \'' +
+      req.body.last_name + '\');',
+      function(err, result) {
+        done();
+
+        if(err) {
+          console.error(err);
+          res.status(406).send('406 Not Acceptable - Username already taken');
+        }else {
+          res.sendStatus(201);
+        }
+      });
+  });
 });
 
-router.delete('/', function(req, res, next) {
-  client.query('DELETE FROM USER WHERE username=' + req.body.username + ' AND pass=' + req.body.pass + ';');
+router.put('/edit/pass', function(req, res, next) {
+  pg.connect(connectionString, function(err, client, done) {
+    client.query('SELECT pass, salt FROM users WHERE username = \'' + req.body.username +'\';',
+     function(err, result) {
+       if(err) {
+         done();
+
+         console.error(err);
+         res.sendStatus(406);
+       }else if(!result || result.rows.length === 0) {
+         done();
+         res.sendStatus(404);
+       }else {
+         var hashpass = 'sha1$' + result.rows[0].salt + '$1$' + result.rows[0].pass;
+         console.log(passHash.verify(req.body.pass, hashpass));
+         if(passHash.verify(req.body.pass, hashpass)) {
+           var hashlist = passHash.generate(req.body.newpass).split('$');
+           client.query('UPDATE users SET pass = \'' + hashlist[3] +
+            '\', salt = \'' + hashlist[1] + '\' WHERE username = \'' +
+            req.body.username + '\';',
+            function(err, result) {
+              done();
+
+              if(err) {
+                console.err(err);
+                res.sendStatus(406);
+              }else {
+                res.sendStatus(202);
+              }
+            });
+        }else {
+          res.sendStatus(406);
+        }
+      }
+    });
+  });
 });
 
 module.exports = router;
