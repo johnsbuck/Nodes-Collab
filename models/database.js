ec58b7dbb5fd218dfe8e5069fc8ff3293dc84a52
James Ortiz <Ortizj2@students.rowan.edu>
2016-03-25 12:49:05 -0400

    Changes for table

--- models/database.js
+++ models/database.js
@@ -9,23 +9,23 @@ var user = 'CREATE TABLE users (username VARCHAR(40) PRIMARY KEY, ' +
                                 'email VARCHAR(40) UNIQUE NOT NULL, ' +
                                 'first_name VARCHAR(40) NOT NULL, last_name VARCHAR(40) NOT NULL);';
 var group = 'CREATE TABLE groups (groupname VARCHAR(40) PRIMARY KEY, ' +
-                                  'dirpath VARCHAR(40) PRIMARY KEY, ' +
-                                  'privacy INTEGER NOT NULL)';
+                                  'dirpath VARCHAR(40) NOT NULL, ' +
+                                  'privacy INTEGER NOT NULL);';
 var user_group_perm = 'CREATE TABLE user_group_perms (username VARCHAR(40) REFERENCES users (username), ' +
                                     'groupname VARCHAR(40) REFERENCES groups (groupname), ' +
                                     'permissions INTEGER NOT NULL, ' +
-                                    'PRIMARY KEY(username, groupname))';
+                                    'PRIMARY KEY(username, groupname));';
 var post = 'CREATE TABLE posts (id SERIAL PRIMARY KEY, ' +
                                 'username VARCHAR(40) REFERENCES users (username), ' +
                                 'timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, ' +
-                                'type INTEGER NOT NULL)';
+                                'type INTEGER NOT NULL);';
 var comment = 'CREATE TABLE comments (id SERIAL PRIMARY KEY, ' +
                                     'username VARCHAR(40) REFERENCES users (username), ' +
                                     'post_id INTEGER REFERENCES posts (id), ' +
                                     'timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, ' +
-                                    'type INTEGER NOT NULL)';
+                                    'type INTEGER NOT NULL);';
 var tag = 'CREATE TABLE tags (post_id INTEGER PRIMARY KEY REFERENCES posts (id), ' +
-                              'tag VARCHAR(40))';
+                              'tag VARCHAR(40));';
 
 var query = client.query(user + group + user_group_perm + post + comment + tag);
 
