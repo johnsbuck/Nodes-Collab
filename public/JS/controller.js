0b699a84bc8fe211dd0ab4946d39b8e50909e41e
James Ortiz <Ortizj2@students.rowan.edu>
2016-03-25 15:01:31 -0400

    Changed GET to POST for controller and index on signin page

--- public/JS/controller.js
+++ public/JS/controller.js
@@ -12,14 +12,21 @@ app.controller('newUserCtrl', function($scope, $http) {
     }
 });
 
-app.controller('loginCtrl', function($scope, $http) {
+app.controller('loginCtrl', function($scope, $http, $location) {
     $scope.sub = function() {
         console.log($scope.formData);
-        $http.get('/login', $scope.formData).
+        $http.post('/login', $scope.formData).
         success(function(data) {
             console.log('Sent to sever successfully.');
+            //window.location.replace('main.html', 'Signin.html');
         }).error(function(data){
             console.log('ERROR: Not sent to server.');
         });
     }
 });
+
+app.controller('settingsCtrl', function($scope, $http) {
+  $scope.sub = function() {
+
+  }
+})
--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,25 @@ var path = require('path');
 
 var connectionString = process.env.DATABASE_URL || 'postgres://jsb:test@localhost/nodesconnect';
 
-router.get('/login', function(req, res, next) {
+router.post('/login', function(req, res, next) {
+  console.log(req.body);
   pg.connect(connectionString, function(err, client, done) {
-    client.query('SELECT pass, salt FROM users WHERE username = \'' + req.body.username +
-                  '\' OR email = \'' + req.body.email + '\';',
+    client.query('SELECT pass, salt FROM users WHERE email = \'' + req.body.email + '\';',
      function(err, result) {
+       console.log(result);
        if(err) {
          console.error(err);
          res.sendStatus(406);
        }else if(!result || result.rows.length === 0) {
          done();
+         console.log(result);
          res.status(404).sendFile(path.join(__dirname, '../public', 'Signin.html'));
        }else {
          var hashpass = 'sha1$' + result.rows[0].salt + '$1$' + result.rows[0].pass;
          done();
 
          if(passHash.verify(req.body.pass, hashpass)) {
+           console.log("test");
            res.status(202).sendFile(path.join(__dirname, '../public','main.html'));
          } else {
            res.status(401).sendFile(path.join(__dirname, '../public', 'Signin.html'));
