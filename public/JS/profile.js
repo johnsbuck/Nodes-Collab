ea3b7258662c8633123e8252e18f35fab4420659
Sebastian Wild <Sebastian Wild>
2016-04-24 20:22:16 -0400

    Further delete connection implementation

--- public/JS/profile.js
+++ public/JS/profile.js
@@ -11,13 +11,14 @@ function deleteProfile(username)
   formData['username'] = sessionStorage.getItem('username');
   formData['pass'] = sessionStorage.getItem('pass');
   console.log(formData);
-  /*
+
   $.ajax({
-    url:'/user/delete/connection'
-    context: formData
+    url:'/user/delete/connection',
+    type: 'DELETE',
+    context: {"username" : formData['username'], "pass" : formData['pass']},
     success: function() {
       console.log("Connection removed!");
     }
   });
-  */
+
 }
